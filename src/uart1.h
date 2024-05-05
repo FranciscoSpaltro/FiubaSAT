34bc073a502dae05b53de7be2a0b0bd2f6dbdd56
Facundo Arballo <farballo@fi.uba.ar>
2024-05-05 18:50:44 -0300

    cambios UART

---
+++ src/uart1.h
@@ -0,0 +1,21 @@
+#ifndef UART1_H
+#define UART1_H
+
+#include "task.h"
+#include "uart2.h"
+#include <queue.h>
+#include "semphr.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+
+extern SemaphoreHandle_t uart1_mutex; // Mutex for UART
+
+uint16_t UART1_puts(const char *s);
+void UART1_setup(void);
+void taskUART1_transmit(void *args __attribute__((unused)));
+void taskUART1_receive(void *args __attribute__((unused)));
+
+#endif /* ifndef UART1_H */
---
+++ src/uart2.h
@@ -0,0 +1,21 @@
+#ifndef UART2_H
+#define UART2_H
+
+#include "task.h"
+#include <queue.h>
+#include "semphr.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
+
+extern SemaphoreHandle_t uart2_mutex; // Mutex for UART
+
+uint16_t UART2_puts(const char *s);
+void UART2_setup(void);
+void UART2_putchar(char ch);
+void taskUART2_transmit(void *args __attribute__((unused)));
+void taskUART2_receive(void *args __attribute__((unused)));
+
+#endif /* ifndef UART2_H */

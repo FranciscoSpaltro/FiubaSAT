5928ff8e6822e7d3ccfeb10c9d25a37c2a87ec96
Fran <fspaltro@fi.uba.ar>
2024-09-06 22:24:06 +0000

    refactor: uso mutex para i2c y cambio a taskYELD para el wait

--- src/i2c.c
+++ src/i2c.c
@@ -1,5 +1,6 @@
 #include "i2c.h"
-#include "uart.h"
+
+SemaphoreHandle_t i2c_mutex;
 
 void i2c_setup(void) {
     rcc_periph_clock_enable(RCC_GPIOB); // Habilitar el reloj para el puerto B
@@ -9,7 +10,7 @@ void i2c_setup(void) {
                   GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN, GPIO_I2C1_SCL | GPIO_I2C1_SDA); // Configurar pines para I2C
 
     i2c_peripheral_disable(I2C1); // Desactivar I2C1 antes de configurar
-    i2c_reset(); // Resetear I2C1
+    rcc_periph_reset_pulse(RST_I2C1);
 
     i2c_set_standard_mode(I2C1); // Configurar modo estándar
     i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_36MHZ); // Configurar frecuencia del reloj a 36 MHz
@@ -17,22 +18,29 @@ void i2c_setup(void) {
     i2c_set_dutycycle(I2C1, I2C_CCR_DUTY_DIV2); // Configurar ciclo de trabajo
     i2c_set_ccr(I2C1, 180); // Configurar CCR para 100 kHz
     i2c_peripheral_enable(I2C1); // Habilitar I2C1
+
+    i2c_mutex = xSemaphoreCreateMutex();
+    if (i2c_mutex == NULL) {
+        print_uart("Error al crear el semáforo\n\r");
+        while(1);
+    }
 }
 
 void i2c_wait_until_ready(void) {
     while (I2C_SR2(I2C1) & I2C_SR2_BUSY) {
-        vTaskDelay(pdMS_TO_TICKS(10)); // Esperar 10 ms
+        taskYIELD();
     }
 }
 
 void i2c_start(uint8_t addr, bool read) {
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     i2c_wait_until_ready();
     i2c_send_start(I2C1);
     while (!(I2C_SR1(I2C1) & I2C_SR1_SB)) {
         // Esperar hasta que el bit de Start esté establecido
         vTaskDelay(pdMS_TO_TICKS(10));
     }
-    print_uart("Start bit establecido\n\r");
+
     i2c_send_7bit_address(I2C1, addr, read ? I2C_READ : I2C_WRITE);
     while (!(I2C_SR1(I2C1) & I2C_SR1_ADDR)) {
         // Esperar hasta que el bit de Address esté establecido
@@ -41,10 +49,12 @@ void i2c_start(uint8_t addr, bool read) {
 
     // Limpiar la bandera de dirección
     (void)I2C_SR2(I2C1);
+    xSemaphoreGive(i2c_mutex);
     
 }
 
 void i2c_write(uint8_t data) {
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     i2c_send_data(I2C1, data);
     // Esperar hasta que se complete la transferencia de datos
     while (!(I2C_SR1(I2C1) & I2C_SR1_BTF)) {
@@ -62,10 +72,12 @@ void i2c_write(uint8_t data) {
         // ACK recibido correctamente
         print_uart("Se recibio ACK\n\r");
     }
+    xSemaphoreGive(i2c_mutex);
 }
 
 
 uint8_t i2c_read(bool last) {
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     if (last) {
         i2c_disable_ack(I2C1);
     } else {
@@ -77,11 +89,14 @@ uint8_t i2c_read(bool last) {
         vTaskDelay(pdMS_TO_TICKS(10));
     }
 
+    xSemaphoreGive(i2c_mutex);
     return i2c_get_data(I2C1);
 }
 
 void i2c_stop(void) {
+    xSemaphoreTake(i2c_mutex, portMAX_DELAY);
     i2c_send_stop(I2C1);
+    xSemaphoreGive(i2c_mutex);
 }
 
 void task_i2c(void *pvParameters) {
@@ -91,44 +106,20 @@ void task_i2c(void *pvParameters) {
     
     for(;;) {
         i2c_start(I2C_SLAVE_ADDRESS, false); // Iniciar comunicación con el Arduino
-        print_uart("Comunicacion I2C iniciada\n\r");
         i2c_write(mensaje);
-        print_uart("Mensaje enviado\n\r");
         i2c_stop();
 
 
         vTaskDelay(pdMS_TO_TICKS(100)); // Esperar a que el Arduino procese el comando
 
-        //i2c_start(I2C_SLAVE_ADDRESS, true); // Leer desde el Arduino
-        //respuesta = i2c_read(true); // Leer la hora (último byte)
-        //i2c_stop();
+        i2c_start(I2C_SLAVE_ADDRESS, true); // Leer desde el Arduino
+        respuesta = i2c_read(true); // Leer la hora (último byte)
+        i2c_stop();
         vTaskDelay(pdMS_TO_TICKS(1000)); // Esperar 1 segundo antes de la próxima solicitud
-        if(respuesta == 0) {
-            mensaje = 1; // Cambiar el comando para solicitar la fecha
-        } else {
-            mensaje = 0; // Cambiar el comando para solicitar la hora
-        }
     }
 }
 
-void i2c_reset(void) {
-    // Deshabilitar I2C
-    I2C_CR1(I2C1) &= ~I2C_CR1_PE;
 
-    // Esperar hasta que I2C esté deshabilitado
-    while (I2C_CR1(I2C1) & I2C_CR1_PE)
-        taskYIELD();
-
-    // Restablecer el I2C
-    if (I2C1 == I2C1) {
-        rcc_periph_reset_pulse(RST_I2C1);
-    } else if (I2C1 == I2C2) {
-        rcc_periph_reset_pulse(RST_I2C2);
-    }
-
-    // Rehabilitar I2C
-    I2C_CR1(I2C1) |= I2C_CR1_PE;
-}
 
 
 

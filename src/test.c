a5b075f62189f0e2c357e80760793ef4df294915
Facundo Arballo <farballo@fi.uba.ar>
2024-09-11 00:02:27 +0000

    rm testing UART1/2 by echo

--- src/test.c
+++ src/test.c
@@ -14,58 +14,6 @@ void taskTest(void *args __attribute__((unused))){
     vTaskDelay(pdMS_TO_TICKS(2000));
 
     taskTestGPS(USART1);
-
-    // Definición de la cadena de prueba
-    char string_1[] = "Testing UART 1\r\n";
-    char string_2[] = "Testing UART 2\r\n";
-    
-    int len_string_1 = strlen(string_1);
-    int len_string_2 = strlen(string_2);
-
-    // Se encola la cadena de prueba en la cola de TX
-    uint16_t nsent_1 = UART_puts(USART1, string_1, pdMS_TO_TICKS(100));
-    uint16_t nsent_2 = UART_puts(USART2, string_2, pdMS_TO_TICKS(100));
-
-    // Se verifica que la cantidad de caracteres enviados sea la correcta
-    if(nsent_1 != sizeof(string_1) - 1){
-        UART_puts(USART3, "Error al enviar datos por UART1. Cantidad caracteres distinta\r\n", pdMS_TO_TICKS(100));
-    }
-
-    // Se verifica que la cantidad de caracteres enviados sea la correcta
-    if(nsent_2 != sizeof(string_2) - 1){
-        UART_puts(USART3, "Error al enviar datos por UART2. Cantidad caracteres distinta\r\n", pdMS_TO_TICKS(100));
-    }
-
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    uint16_t data;
-    int i = 0;
-    // Recibe los datos de la cola de RX y los compara con la cadena de prueba
-    while(UART_receive(USART1, &data)){
-        if(string_1[i] != (char)data || i == len_string_1) {
-            break;
-        }
-        i++;
-    }
-
-    if(i == len_string_1)
-        UART_puts(USART3, "Test UART1 runned successfully\r\n", pdMS_TO_TICKS(100));
-    else
-        UART_puts(USART3, "Error al recibir datos por UART1.\r\n", pdMS_TO_TICKS(100));
-
-    i = 0;
-    // Recibe los datos de la cola de RX y los compara con la cadena de prueba
-    while(UART_receive(USART2, &data)){
-        if(string_2[i] != (char)data || i == len_string_2) {
-            break;
-        }
-        i++;
-    }
-
-    if(i == len_string_2)
-        UART_puts(USART3, "Test UART2 runned successfully\r\n", pdMS_TO_TICKS(100));
-    else
-        UART_puts(USART3, "Error al recibir datos por UART2.\r\n", pdMS_TO_TICKS(100));
-    
     vTaskDelete(NULL);
 }
 
@@ -92,7 +40,6 @@ void taskTestGPS(uint32_t usart_id) {
 }
 
 void taskPrintBuffer(void *args __attribute__((unused))) {
-
     for (;;) {
         UART_print_buffer(USART1);
         UART_print_buffer(USART2);
